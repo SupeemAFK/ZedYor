129acf5e03d8b1a3c04c842248ab7cad540e5324
MeldyRose <praewa241148@gmail.com>
2026-03-25 21:42:02 +0700

    Fixing first and second reports

--- treatment/report1_prisoner_treatment_experience.sql
+++ treatment/report1_prisoner_treatment_experience.sql
@@ -1,13 +1,18 @@
---Show all prisoner who had taken treatment before
 --67070503432 Praewa Thuwatharanimitkul
+--Filter prisoner who had a treatment between date 2026-03-22 to 2026-03-26
 SELECT diagnose_date as "Diagnose Date", 
 pe.first_name as "Prisoner First Name", 
 pe.last_name as "Prisoner Last Name",
-n.first_name as "Nurse First Name", 
-n.last_name as "Nurse Last Name", 
+pne.first_name as "Nurse First Name", 
+pne.last_name as "Nurse Last Name", 
 description as "Description"
 FROM treatment t
-JOIN nurse n on n.id = t.nurse_id
-JOIN prisoner p on p.id = t.prisoner_id
-JOIN person pe on pe.id = p.person_id
+--Getting prisoner name
+JOIN prisoner p ON p.id = t.prisoner_id
+JOIN person pe ON pe.id = p.person_id
+--Getting nurse name
+JOIN nurse n ON n.id = t.nurse_id
+JOIN person pne ON pne.id = n.person_id
+
+WHERE t.diagnose_date >= '2026-03-22' AND t.diagnose_date < '2026-03-26'
 ORDER BY diagnose_date desc;
--- treatment/report2_medicine_prescription_experience.sql
+++ treatment/report2_medicine_prescription_experience.sql
@@ -1,10 +1,9 @@
---Show all medicine those had been used in the medical prescription
 --67070503432 Praewa Thuwatharanimitkul
-SELECT m.code as "Code",
-m.name as "Name", 
-m.generic_name as "Generic Name",
-COUNT(*) as "Usage Count", m.caution as "Caution"
+--Filtering by the caution of medicine to see if the medicine need to be taken by avoiding specific things.
+--Showing only medicine those had been assigned in the prescription.
+SELECT m.code as "Code",m.name as "Name", m.generic_name as "Generic Name",COUNT(*) as "Usage Count", m.caution as "Caution"
 FROM medicationprescription mp
 JOIN medicine m on m.id = mp.medicine_id
+WHERE m.caution in ( 'Avoid grapefruit','Avoid potassium supplements','Do not use if pregnant','Do not stop abruptly','Caution with alcohol')
 GROUP BY m.name,m.generic_name,m.code,m.caution
 ORDER BY m.code asc,m.name asc;
