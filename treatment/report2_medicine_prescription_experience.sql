d5cb2c6a3836bc7da950709e3168368c6c2f6cb0
MeldyRose <praewa241148@gmail.com>
2026-03-26 01:38:37 +0700

    Fixing 2 and 3 due to new data

--- treatment/report2_medicine_prescription_experience.sql
+++ treatment/report2_medicine_prescription_experience.sql
@@ -1,9 +1,9 @@
 --67070503432 Praewa Thuwatharanimitkul
 --Filtering by the caution of medicine to see if the medicine need to be taken by avoiding specific things.
---Showing only medicine those had been assigned in the prescription.
+--Showing only medicine those had been assigned in the prescription with an assigned count to count how many time the medicine has been assigned.
 SELECT m.code as "Code",m.name as "Name", m.generic_name as "Generic Name",COUNT(*) as "Usage Count", m.caution as "Caution"
 FROM medicationprescription mp
 JOIN medicine m on m.id = mp.medicine_id
-WHERE m.caution in ( 'Avoid grapefruit','Avoid potassium supplements','Do not use if pregnant','Do not stop abruptly','Caution with alcohol')
+WHERE m.caution LIKE '%Take%'
 GROUP BY m.name,m.generic_name,m.code,m.caution
 ORDER BY m.code asc,m.name asc;
